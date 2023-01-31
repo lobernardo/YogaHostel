787f6e532c80d46360089ef02f88800f90eca490
Leo Bernardo <leo.olivbernardo@gmail.com>
2023-01-31 08:54:43 -0300

    commit_31_2

---
+++ yoga/src/components/layout/SlideShowHomeSection/SlideShow.js
@@ -0,0 +1,16 @@
+import styles from './SlideShow.module.css'
+
+
+function SlideShow (){
+  
+    return(
+
+    <div className={styles.hometwo__slideshow}>
+
+
+    </div>
+
+    )
+}
+
+export default SlideShow
--- yoga/src/components/pages/HomeSection/HomeTwo.js
+++ yoga/src/components/pages/HomeSection/HomeTwo.js
@@ -2,6 +2,7 @@ import styles from './HomeTwo.module.css'
 import logo_apoio from '../../assets/logo_apoio.png'
 import yoga1 from '../../assets/yoga1.png'
 
+
 function HomeTwo (){
     return(
 
@@ -12,6 +13,7 @@ function HomeTwo (){
             <p>Yoga & meditação</p>
             <p>Desde 1999 oferecendo um verdadeiro retiro de paz e calmaria no coração da serra da Bocaína.</p>
             <img className={styles.banner_apoio_home} src={yoga1}/>
+         
         </div>
     </div>
 
