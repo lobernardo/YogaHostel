fd269f733f8471b7b64dbc0517d143daed01349e
Leo Bernardo <leo.olivbernardo@gmail.com>
2023-02-19 19:14:37 -0300

    commint_initial_map

---
+++ yoga/src/components/pages/HomeSection/ComoChegar.js
@@ -0,0 +1,8 @@
+function ComoChegar() {
+    return (
+      <div>
+        <h1>Mapa de como chegar</h1>
+        <Localization destination={{ lat: -23.550520, lng: -46.633308 }} />
+      </div>
+    );
+  }
--- yoga/src/components/pages/HomeSection/Localization.js
+++ yoga/src/components/pages/HomeSection/Localization.js
@@ -1,19 +1,65 @@
 import styles from './Localization.module.css'
 
+import { useState, useEffect } from 'react';
+import { GoogleMap, LoadScript, DirectionsService, DirectionsRenderer } from '@googlemaps/api';
 
 
-const Localization = () => {
-  return (
-    <div className={styles.map__container}>
-      
-        <h1>Veja como chegar</h1>
 
-        <div className={styles.map__section}>
+function Localization() {
 
-        </div>
-    </div>
-    
+  const [map, setMap] = useState(null);
+  const [directionsResponse, setDirectionsResponse] = useState(null);
+  const [origin, setOrigin] = useState(null);
+  const [destination, setDestination] = useState(null);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setOrigin({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
+  const onLoad = (map) => {
+    setMap(map);
+  };
+
+
+return (
+    <LoadScript googleMapsApiKey="YOUR_API_KEY">
+      <GoogleMap
+        mapContainerStyle={{ height: '400px' }}
+        center={origin}
+        zoom={14}
+        onLoad={onLoad}
+      >
+        {origin && destination && (
+          <DirectionsService
+            options={{
+              destination: destination,
+              origin: origin,
+              travelMode: 'DRIVING',
+            }}
+            callback={(result) => {
+              if (result !== null) {
+                setDirectionsResponse(result);
+              }
+            }}
+          />
+        )}
+        {directionsResponse && (
+          <DirectionsRenderer
+            options={{
+              directions: directionsResponse,
+            }}
+          />
+        )}
+      </GoogleMap>
+    </LoadScript>
   );
-};
+}
 
 export default Localization
